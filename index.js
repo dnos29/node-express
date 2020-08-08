642ba73fbf5df37a414b57bf286913fd1fa9e26e
sondn.da.ubuntu1804 <sondn.da@gmail.com>
2020-08-08 14:57:14 +0000

    const port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 
 const app = express();
+const PORT = 3000;
 
 app.get('/',(req, res, next) => {
  res.send({status: 'ok'})
 })
-app.listen(3000, function(){
-    console.log('Server listening on port:3000...')
+app.listen(PORT, function(){
+    console.log(`Server listening on port:${PORT}...`)
 })
